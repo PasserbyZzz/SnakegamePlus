667521ab05fbbbf781a6e191871f51f0e9552ce8
PasserbyZzz <2751689774@qq.com>
2024-07-09 21:41:08 +0800

    enter user's name

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include "game.h"
+#include "curses.h"
 #include <string>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << "Welcome to The Snake Game!" << endl;
-    cout << "Please enter your name: ";
-    string name;
-    getline(cin, name);
+    //输入玩家姓名
+    initscr(); //初始化curses模式
+    raw();
+    curs_set(0);
+    char* welcome = "Welcome to The Snake Game!";
+    mvprintw(LINES / 2 - 1, (COLS - strlen(welcome)) / 2, welcome);
+    char* player = "Please enter your name: ";
+    mvprintw(LINES / 2, (COLS - strlen(player)) / 2, player);
+    refresh();
+    char name[7];
+    mvgetstr(LINES / 2, (COLS - strlen(player)) / 2 + strlen(player), name); //玩家输入姓名
+    string s(name);
+    endwin(); //结束curses模式
+
     Game game;
-    game.setName(name);
+    game.setName(s);
     game.startGame();
 }
